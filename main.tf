8cbffa1442ab5ee18271c5a96287e33e584d20f9
rajagopalanrg <56252656+rajagopalanrg@users.noreply.github.com>
2022-01-23 17:21:07 +0530

    added backend

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ variable "rg_name" {}
 variable "storage_name" {}
 variable "storage_tier" {}
 variable "storage_repl_type" {}
+variable "tfstatefilename" {}
 
 provider "azurerm" {
   
@@ -10,6 +11,14 @@ provider "azurerm" {
     resource_group {
       prevent_deletion_if_contains_resources = true
     }
+  }
+}
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "learning"
+    storage_account_name = "terraformstatefilend"
+    container_name       = "statefiles"
+    key                  = var.tfstatefilename
   }
 }
 
