9dedae6e09a782eb979dbc4b54dfc72923762a90
Vaishu <vaisumathy@gmail.com>
2022-01-23 20:44:26 +0530

    updated all for subscription

--- main.tf
+++ main.tf
@@ -15,10 +15,7 @@ provider "azurerm" {
 }
 terraform {
   backend "azurerm" {
-    resource_group_name  = "learning"
-    storage_account_name = "terraformstatefilend"
     container_name       = "statefiles"
-
   }
 }
 
